67f4be1a35bc216cc30f5ed1c42bf43a1e570600
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-03-25 13:50:28 +0530

    Basic Player Movement

---
+++ BadBoxArt/BadBoxArt/Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,25 @@
+﻿
+using UnityEngine;
+ using System.Collections;
+ 
+ public class PlayerMove : MonoBehaviour {
+     
+     float speed = 5.0f;
+     
+     void Update() {
+
+		if (Input.GetKeyDown (KeyCode.LeftShift)) {
+
+			speed = 10.0f;
+		}
+
+		if (Input.GetKeyUp (KeyCode.LeftShift)) {
+
+			speed = 5.0f;
+		
+		}
+
+         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+         transform.position += move * speed * Time.deltaTime;
+     }
+ }
